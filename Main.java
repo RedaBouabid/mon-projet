72a4c4402d399b412ac4b9098179c022391d3878
Bouabid Reda <bouabidreda170@gmail.com>
2026-09-24 19:11:26 +0000

    Deuxième amélioration

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Premiere amelioration du projet");
+        System.out.println("deuxieme amelioration du projet");
     }
 }
